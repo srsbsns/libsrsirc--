0a7ca6aed9a61ccd14aacc5817cffb65c4407136
Learath2 <learath2@gmail.com>
2014-04-23 12:04:46 +0300

    Added a really sloppy test

--- src/testlibsrsircpp.cpp
+++ src/testlibsrsircpp.cpp
@@ -10,6 +10,23 @@
 int
 main(int argc, char **argv)
 {	
-	std::cout << "Absolutely no idea what to put here... <-- something simple which demonstrates how to use the lib." << std::endl;
+	IRC handle;
+	handle.Nick("Testircpp");
+	handle.Uname("Testircpp");
+	handle.ConFlags(0);
+	handle.RegisterCallbackConRead(ConRead, 0);
+	handle.Server("irc.quakenet.org", "6667");
+	if(!handle.Connect(10*1000000UL)){
+		cout<<"Failed to connect"<<endl;
+		return -1;
+	}
+	if(!handle.Write("JOIN #Lea2")){
+		cout<<"Couldn't join"<<endl;
+		return -1;
+	}
+	if(!handle.Write("PRIVMSG #Lea2 :Teeeest")){
+		cout<<"Couldn't Write"<<endl;
+		return -1;
+	}
 	return EXIT_SUCCESS;
 }
